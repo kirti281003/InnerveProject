b1785e1df9d32840943ef99dc468b9ff1c6d21ad
Kirti Aggarwal <kirti057btcse21@igdtuw.ac.in>
2022-07-18 00:26:25 -0700

    validatons

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express=require ("express");
 const bodyParser=require("body-parser");
 const mongoose=require("mongoose");
 const bcrypt=require("bcrypt");
+const { body, validationResult } = require('express-validator');
+var msg="";
 const app=express();
 app.set('view engine','ejs');
 app.use(express.static("public"));
@@ -9,12 +11,12 @@ app.use(bodyParser.urlencoded({extended: true}));
 mongoose.connect("mongodb://127.0.0.1:27017/innerveDb",{useNewUrlParser:true});
 const registerSchema=new mongoose.Schema(
   {
-    Name:String,
-    Email:String,
-    College:String,
-    Year:Number,
-    Phone:Number,
-    Password:String
+   Name:String,
+   Email:String,
+   College:String,
+   Year:Number,
+   Phone:Number,
+   Password:String
   }
 );
 const Register=mongoose.model("Register",registerSchema);
@@ -23,7 +25,7 @@ app.get("/",(req,res)=>
     res.render("home");
 })
 app.get("/register",(req,res)=>
-{res.render("register");
+{res.render("register",{msg:msg});
 
 });
 app.get("/registered",(req,res)=>
@@ -55,30 +57,40 @@ app.post("/ticket",(req,res)=>{
         }
     })
 })
-app.post("/register",(req,res)=>{
-    var saltround=8;
-    bcrypt.hash(req.body.password,saltround,(err,hash)=>
-    {
-        if(!err)
+app.post("/register",
+[
+    body("email").isEmail(),
+    body('password').isLength({ min:6 })
+],(req,res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        msg=errors.array()[0].msg+":"+errors.array()[0].param;
+        res.render("register",{msg:msg});
+      
+    }
+    else{
+        var saltround=8;
+        bcrypt.hash(req.body.password,saltround,(err,hash)=>
         {
-            var password=hash;
-            const r=new Register(
-                {
-                    Name:req.body.name,
-                    Email:req.body.email,
-                    College:req.body.college,
-                    Year:req.body.year,
-                    Phone:req.body.phone,
-                    Password:password
-        
-                }
-            )
-            r.save();
-            res.redirect("/");
-        }
-    })
-
-
+            if(!err)
+            {
+                var password=hash;
+                const r=new Register(
+                    {
+                        Name:req.body.name,
+                        Email:req.body.email,
+                        College:req.body.college,
+                        Year:req.body.year,
+                        Phone:req.body.phone,
+                        Password:password
+            
+                    }
+                )
+                r.save();
+                res.redirect("/");
+            }})
+    }
+    msg="";
 
 });
 app.listen(3000,()=>
